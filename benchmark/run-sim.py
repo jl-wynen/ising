08847b92d2ddef183b4d7ca22f4757fd2cf9d3c0
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-10-08 16:14:01 +0200

    Add script to benchmark py and c++ impls

---
+++ benchmark/run-sim.py
@@ -0,0 +1,131 @@
+"""
+Benchmark different implementations.
+
+Automatically adjusts lattice size and temperature but does not change
+the number of sweeps. They need to be set in the base implementations manually.
+"""
+
+from pathlib import Path
+import subprocess
+import tempfile
+import re
+import pickle
+import shutil
+
+# output file for timings
+BENCHMARK_FILE = Path(__file__).parent/"ising.ben"
+
+# source files
+PYTHON_SRC = Path(__file__).parent/"../python/ising.py"
+CPP_SRC = Path(__file__).parent/"../cpp/ising.cpp"
+
+def run_python(sizes, wdir):
+    "Run the Python implementation."
+
+    # regexes to extract and replace stuff
+    nxre = re.compile(r"NX = (\d+).*$")
+    nyre = re.compile(r"NY = (\d+).*$")
+    tempre = re.compile(r"TEMPERATURES = ([^\#]+)(\#.*)?$")
+    timere = re.compile(r"Duration in wall clock time: ([\d\.]+)s")
+
+    # read soruce code
+    with open(PYTHON_SRC, "r") as srcf:
+        src = srcf.read()
+
+    # script to run
+    script = wdir/"ising.py"
+
+    times = []
+    for size in sizes:
+        # insert size and fix temperature
+        src = "\n".join(re.sub(nxre, f"NX = {size}",
+                               re.sub(nyre, f"NY = {size}",
+                                      re.sub(tempre, "TEMPERATURES = [1.]", line)))
+                        for line in src.split("\n"))
+        with open(script, "w") as outf:
+            outf.write(src)
+
+        # run the benchmark
+        proc = subprocess.run(["python3", str(script), str(wdir/"data")],
+                              capture_output=True)
+
+        # extract run time
+        for line in proc.stdout.decode("utf-8").split("\n"):
+            match = re.match(timere, line)
+            if match:
+                times.append(float(match[1]))
+                break
+
+    return times
+
+def run_cpp(sizes, wdir):
+    "Run the C++ implementation/"
+
+    # regexes to extract and replace stuff
+    nxre = re.compile(r"constexpr Index NX = (\d+);.*$")
+    nyre = re.compile(r"constexpr Index NY = (\d+);.*$")
+    timere = re.compile(r"Duration in wall clock time: ([\d\.]+)s")
+
+    # read source code
+    with open(CPP_SRC, "r") as srcf:
+        src = srcf.read()
+
+    # temporary files to run the program
+    cmake_file_in = CPP_SRC.parent/"CMakeLists.txt"
+    cmake_file = wdir/"CMakeLists.txt"
+    build_dir = wdir/"build"
+    source = wdir/"ising.cpp"
+
+    # copy over cmake file and run cmake
+    shutil.copy(cmake_file_in, cmake_file)
+    shutil.copy(CPP_SRC, source)
+    build_dir.mkdir()
+    subprocess.run(["cmake", ".."], cwd=build_dir)
+
+    times = []
+    for size in sizes:
+        # insert size and fix temperature
+        src = "\n".join(re.sub(nxre, f"constexpr Index NX = {size};",
+                               re.sub(nyre, f"constexpr Index NY = {size};",
+                                      line.replace("constexpr auto temperatures = listTemperatures();",
+                                                   "constexpr std::array<double,1> temperatures{1.,};")))
+                        for line in src.split("\n"))
+        # write source and compile
+        with open(source, "w") as outf:
+            outf.write(src)
+        subprocess.run(["make"], cwd=build_dir)
+
+        # run the program
+        proc = subprocess.run(["ising", str(wdir/"data")], cwd=build_dir,
+                              capture_output=True)
+
+        # extract run time
+        for line in proc.stdout.decode("utf-8").split("\n"):
+            match = re.match(timere, line)
+            if match:
+                times.append(float(match[1]))
+                break
+    return times
+
+def main():
+    "Run the benchmarks."
+
+    sizes = [4, 16, 24, 32, 48]
+
+    times = dict()
+    with tempfile.TemporaryDirectory() as wdir:
+        wdir = Path(wdir)
+        times["python"] = run_python(sizes, wdir)
+        times["c++"] = run_cpp(sizes, wdir)
+
+    # save benchmark to file
+    pickle.dump({"xlabel": "Nx*Ny",
+                 "ylabel": "time / s",
+                 "xvalues": [size**2 for size in sizes],
+                 "yvalues": times},
+                open(BENCHMARK_FILE, "wb"))
+
+
+
+if __name__ == "__main__":
+    main()

20e0db93c9ca82a90b695057ff3e16181a19bff6
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-10-12 21:16:36 +0200

    Add rng.hpp

---
+++ cpp/dim-n/rng.hpp
@@ -0,0 +1,60 @@
+#ifndef ISING_RNG_HPP
+#define ISING_RNG_HPP
+
+#include <random>
+
+#include "lattice.hpp"
+#include "configuration.hpp"
+
+/// Helper class to handle a random number generator.
+struct Rng
+{
+    /// Seed the rng and set up distributions.
+    explicit Rng(unsigned long const seed)
+        : rng{seed}, indexDist{0, LATSIZE.get()-1}, realDist{0, 1}, spinDist{0, 1}
+    { }
+
+    /// Generate a random index into a configuration.
+    Index genIndex()
+    {
+        return Index{indexDist(rng)};
+    }
+
+    /// Generate a random double in [0, 1).
+    double genReal()
+    {
+        return realDist(rng);
+    }
+
+    /// Generate a random spin, one of {-1, +1}.
+    Spin genSpin()
+    {
+        return spinDist(rng)==0 ? Spin{-1} : Spin{1};
+    }
+
+private:
+    /// The generator.
+    std::mt19937 rng;
+
+    /// Distribution to generator lattice indices.
+    std::uniform_int_distribution<typename Index::Underlying> indexDist;
+
+    /// Distribution to generate floating point numbers in [0, 1).
+    std::uniform_real_distribution<double> realDist;
+
+    /// Distribution to generate spins, i.e. values 0 or 1.
+    std::uniform_int_distribution<typename Spin::Underlying> spinDist;
+};
+
+
+/// Generate a random spin configuration.
+Configuration randomCfg(Rng &rng)
+{
+    Configuration cfg;
+    for (Spin &s : cfg) {
+        s = rng.genSpin();
+    }
+    return cfg;
+}
+
+#endif  // ndef ISING_RNG_HPP

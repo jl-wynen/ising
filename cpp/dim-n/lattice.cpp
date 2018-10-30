87179f6b15bbbb17095791851c02d95d6e28737f
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-10-30 16:14:51 +0100

    Add function to build neighbour indices

---
+++ cpp/dim-n/lattice.cpp
@@ -0,0 +1,99 @@
+#include "lattice.hpp"
+
+#include <array>
+
+#include "ndebug.hpp"
+
+namespace detail_
+{
+    namespace
+    {
+        /// Increment multi dimensional index.
+        void increment(std::array<Index, NDIM.get()> &index)
+        {
+            for (Index i = 0_i; i < NDIM; ++i) {
+                // try to increment at position i
+                if (++index[i.get()] == LATSHAPE[i.get()]) {
+                    // back off, increment was too much
+                    index[i.get()] = 0_i;
+                }
+                else {
+                    // it is fine, keep the index
+                    return;
+                }
+            }
+            // wrap around and start from 0 again
+            index[0] = 0_i;
+        }
+
+        /// Increment the index at a given position taking PBCs into account.
+        std::array<Index, NDIM.get()> incrementedAt(
+            std::array<Index, NDIM.get()> const &index,
+            Index const pos)
+        {
+            if constexpr (not ndebug) {
+                if (pos > NDIM) {
+                    throw std::out_of_range("Index position to increment out of range");
+                }
+            }
+
+            std::array<Index, NDIM.get()> incremented{index};
+            if (index[pos.get()] == LATSHAPE[pos.get()]-1_i) {
+                incremented[pos.get()] = 0_i;
+            }
+            else {
+                incremented[pos.get()] = index[pos.get()]+1_i;
+            }
+            return incremented;
+        }
+
+        /// Decrement the index at a given position taking PBCs into account.
+        std::array<Index, NDIM.get()> decrementedAt(
+            std::array<Index, NDIM.get()> const &index,
+            Index const pos)
+        {
+            if constexpr (not ndebug) {
+                if (pos > NDIM) {
+                    throw std::out_of_range("Index position to decrement out of range");
+                }
+            }
+
+            std::array<Index, NDIM.get()> decremented{index};
+            if (index[pos.get()] == 0_i) {
+                decremented[pos.get()] = LATSHAPE[pos.get()]-1_i;
+            }
+            else {
+                decremented[pos.get()] = index[pos.get()]-1_i;
+            }
+            return decremented;
+        }
+
+        /// Compute the flat index from a set of NDIM indices.
+        Index totalIndex(std::array<Index, NDIM.get()> const &index)
+        {
+            Index total{index[0]};
+            for (Index i = 1_i; i < NDIM; ++i) {
+                total = total*LATSHAPE[i.get()] + index[i.get()];
+            }
+            return total;
+        }
+    }
+
+    std::vector<Index> makeNeighbourList()
+    {
+        std::vector<Index> neighbours((2_i*NDIM*LATSIZE).get());
+        std::array<Index, NDIM.get()> index;
+        std::fill(std::begin(index), std::end(index), 0_i);
+
+        for (Index i = 0_i; i < LATSIZE; ++i) {
+            for (Index d = 0_i; d < NDIM; ++d) {
+                neighbours[(2_i*NDIM*i + d).get()] = totalIndex(incrementedAt(index, d));
+                neighbours[(2_i*NDIM*i + d+1_i).get()] = totalIndex(decrementedAt(index, d));
+            }
+
+            increment(index);
+        }
+
+        return neighbours;
+    }
+}
--- cpp/dim-n/lattice.hpp
+++ cpp/dim-n/lattice.hpp
@@ -2,83 +2,35 @@
 #define ISING_LATTICE_HPP
 
 #include <array>
-#include <cstddef>
-#include <utility>
 #include <vector>
 
-#include "arithmetic.hpp"
-#include "ndebug.hpp"
+#include "index.hpp"
 
-/// Type for indexes into configurations.
-using Index = ArithmeticType<size_t, struct IndexTag>;
-
-constexpr Index operator""_i(unsigned long long const x) noexcept
-{
-    return Index{x};
-}
-
-namespace {
-    // overly complicated but awesome way to compute the prouct of all elements in an array
-    template <typename Array, size_t ...Idxs>
-    constexpr Index prod_impl(Array const &array, std::index_sequence<Idxs...>)
-    {
-        return (array[Idxs]*...);
-    }
-    template <typename T, size_t N>
-    constexpr Index prod(std::array<T, N> const &array)
-    {
-        return prod_impl(array, std::make_index_sequence<N>());
-    }
-}
-
-
-constexpr Index NDIM {2};
+/// Number of dimensions.
+constexpr Index NDIM{2};
+/// Shape of the lattice, NDIM numbers.
 constexpr std::array<Index, NDIM.get()> LATSHAPE{Index{4ul},
                                                  Index{3ul}};
-constexpr Index LATSIZE = prod(LATSHAPE);
 
+/// Total lattice size.
+constexpr Index LATSIZE = [](auto &shape) {
+                              Index size{1};
+                              for (Index d : shape) {
+                                  size = size*d;
+                              }
+                              return size;
+                          }(LATSHAPE);
 
-namespace {
-    template <size_t dim, typename ...Idxs>
-    constexpr Index totalIdx_impl(Index const head, Idxs const ...tail) noexcept(ndebug)
-    {
-        if constexpr (not ndebug) {
-            if (head >= LATSHAPE[dim])  // Index is unsigned => this checks also for 'head < 0'.
-                throw std::out_of_range("Index is out of range.");
-        }
-
-        if constexpr (dim < NDIM.get()-1) {
-             return head + LATSHAPE[dim]*(totalIdx_impl<dim+1>(tail...));
-        }
-        else {
-            return head;
-        }
-    }
+namespace detail_ {
+    /// Build the neighbour index list.
+    std::vector<Index> makeNeighbourList();
 }
 
-/// Compute the total lattice index from individual indices for each dimension.
-/**
- * The layout is 'column-major' in the sense that the first index is fastest running.
- */
-template <typename ...Idxs>
-constexpr Index totalIdx(Idxs const ...idxs) noexcept(ndebug)
+/// Return the list of neares neighbour indices.
+inline std::vector<Index> const &neighbourList()
 {
-    static_assert(sizeof...(Idxs) == NDIM.get(),
-                  "Need to pass as many indices as there are dimensions.");
-    static_assert((std::is_same_v<Idxs, Index>&&...),
-                  "Function only accepts Index as parameter.");
-
-    return totalIdx_impl<0>(idxs...);
-}
-
-/// Apply periodic boundary conditions to idx.
-constexpr Index applyPeriodicBC(Index const idx, Index const n) noexcept
-{
-    if (idx == n)
-        return 0_i;
-    else if (idx == static_cast<Index>(-1))
-        return n-1_i;
-    return idx;
+    static std::vector<Index> neighbours{detail_::makeNeighbourList()};
+    return neighbours;
 }
 
 #endif  // ndef ISING_LATTICE_HPP

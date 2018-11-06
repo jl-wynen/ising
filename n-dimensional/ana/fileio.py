3fcd70225870faf950aeb5f5dc048a7e86152cdc
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-11-06 15:11:40 +0100

    Add analysis helper for file IO

---
+++ n-dimensional/ana/fileio.py
@@ -0,0 +1,29 @@
+import re
+
+import numpy as np
+
+class Metadata:
+    "Store metadata on ensembles."
+
+    def __init__(self, J, h, shape):
+        self.J = J
+        self.h = h
+        self.shape = shape
+
+def loadMetadata(fname):
+    "Load metadata from first line of file."
+
+    paramStr = open(fname, "r").readline()
+
+    match = re.match(r"# J=([^ ]+) h=([^ ]+) shape=\[(\d+(, \d+)*)\]", paramStr)
+    if not match or len(match.groups()) < 4:
+        raise RuntimeError(f"Invalid first line in file {fname}: '{paramStr}'")
+
+    return Metadata(float(match[1]),
+                    float(match[2]),
+                    [int(d) for d in match[3].split(",")])
+
+def loadFile(fname):
+    "Load meta- and 'normal' data from file."
+    return loadMetadata(fname), \
+        np.loadtxt(fname, skiprows=1, delimiter=",")

cd3f5bafbfe7056800d0767c851ade83d305da31
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-11-06 15:13:06 +0100

    Use fileio in plot script

--- n-dimensional/ana/plot.py
+++ n-dimensional/ana/plot.py
@@ -1,32 +1,10 @@
 import sys
 from pathlib import Path
-import re
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Metadata:
-    def __init__(self, J, h, shape):
-        self.J = J
-        self.h = h
-        self.shape = shape
-
-def loadMetadata(fname):
-    paramStr = open(fname, "r").readline()
-
-    match = re.match(r"# J=([^ ]+) h=([^ ]+) shape=\[(\d+(, \d+)*)\]", paramStr)
-    if not match or len(match.groups()) < 4:
-        raise RuntimeError(f"Invalid first line in file {fname}: '{paramStr}'")
-
-    return Metadata(float(match[1]),
-                    float(match[2]),
-                    [int(d) for d in match[3].split(",")])
-
-def loadFile(fname):
-    meta = loadMetadata(fname)
-    energy, magn = np.loadtxt(fname, skiprows=1, delimiter=",")
-    return meta, energy, magn
-
+from fileio import loadFile
 
 def main():
     if len(sys.argv) != 2:
@@ -38,10 +16,10 @@ def main():
     energy = []
     magnetisation = []
     h = []
-    for fname in datadir.iterdir():
-        meta, e, m = loadFile(fname)
-        e = np.mean(e)
-        m = np.mean(m)
+    for fname in [d for d in datadir.iterdir() if d.suffix=='.dat']:
+        meta, dat = loadFile(fname)
+        e = np.mean(dat[0])
+        m = np.mean(dat[1])
         energy.append(e)
         magnetisation.append(np.abs(m))
         h.append(meta.h)

3da88d4b9fab0bf8b3547be5fec6562776bfe5b6
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-11-27 09:17:03 +0100

    Add plot and fit routines for correlators

--- n-dimensional/ana/plot.py
+++ n-dimensional/ana/plot.py
@@ -2,11 +2,12 @@ import sys
 from pathlib import Path
 from collections import Counter
 from operator import itemgetter
+import itertools
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from fileio import loadFile
+from fileio import loadFile, loadCorrFile
 
 def findVarying(params):
     "Return index of varying parameter. There must be exactly 1, the others must be fixed."
@@ -58,6 +59,10 @@ def meanAndErr(x, nbs=100, bslength=None):
 
     return np.mean(x), np.std(np.mean(x[bootstrapIndices], axis=1), axis=0)
 
+def computeCorrelator(corr, magn, magnErr):
+    cMean, cerr = meanAndErr(corr)
+    return cMean-magn**2, np.sqrt(cerr**2 + 4*magn**2*magnErr**2)
+
 def loadData(datadir, skip=1):
     """
     Load parameters, energies, and magnetisations from all datafiles in directory.
@@ -68,14 +73,76 @@ def loadData(datadir, skip=1):
 
     metas = []
     energies = []
-    magns = []
-    for m, dat in map(loadFile, filter(lambda d: d.suffix == ".dat", datadir.iterdir())):
-        metas.append(m)
-        energies.append(meanAndErr(dat[0, ::skip]/m.latsize()))
-        magns.append(meanAndErr(np.abs(dat[1, ::skip])))
+    magnetisations = []
+    correlators = []
+    for fname in filter(lambda d: d.suffix == ".dat", datadir.iterdir()):
+        meta, dat = loadFile(fname)
+        meta2, dists, corrs = loadCorrFile(fname.with_suffix(".corr"))
+        assert meta == meta2
+
+        metas.append(meta)
+        energies.append(meanAndErr(dat[0, ::skip]/meta.latsize()))
+        magnetisations.append(meanAndErr(dat[1, ::skip]))
+
+        correlators.append([(d, computeCorrelator(c[::skip], *magnetisations[-1]))
+                            for d, c in zip(dists, corrs)])
 
     (xlabel, x), fixed = splitVarFixed(metas)
-    return (xlabel, x), fixed, energies, magns
+    return (xlabel, x), fixed, energies, magnetisations, correlators
+
+def plotEM(xlabel, x, fixed, energies, magns):
+    fig = plt.figure(figsize=(11, 5))
+    fig.suptitle(",   ".join("{}={}".format(n, v) for n, v in fixed.items()))
+
+    axe = fig.add_subplot(121)
+    axe.set_xlabel(xlabel)
+    axe.set_ylabel(r"$E / \Lambda$")
+    xs, es = xsorted(x, energies)
+    axe.errorbar(xs, *zip(*es), ls="-", marker=".")
+
+    axm = fig.add_subplot(122)
+    axm.set_xlabel(xlabel)
+    axm.set_ylabel(r"$|m|$")
+    xs, ms = xsorted(x, magns)
+    axm.errorbar(xs, *zip(*ms), ls="-", marker=".")
+
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+def fitCorr(x, y, err):
+    x = np.ma.masked_where(y<=0, x)
+    y = np.ma.masked_where(y<=0, y)
+
+    ly = np.log(y)
+    poly = np.polyfit(x, ly, 1, w=1/err)
+
+    xdraw = np.linspace(x[0], x[-1], 200)
+    ydraw = np.exp(poly[0]*xdraw + poly[1])
+    corrLength = -1.0 / poly[0]
+
+    return corrLength, xdraw, ydraw
+
+def plotCorr(xlabel, x, fixed, corrs):
+    fig = plt.figure(figsize=(5.5, 5))
+    fig.suptitle(",   ".join("{}={}".format(n, v) for n, v in fixed.items()))
+
+    ax = fig.add_subplot(111)
+    ax.set_xlabel(r"r")
+    ax.set_ylabel(r"$g(r) / g(0)$")
+    for x, c, col, marker in zip(*xsorted(x, corrs),
+                                 itertools.cycle(("C0", "C1", "C2", "C3", "C4", "C5")),
+                                 itertools.cycle(("^", "x", "_", ".", "d", "v"))):
+        dist, corr = zip(*c)
+        mean, err = zip(*corr)
+        mean, err = np.array(mean), np.array(err)
+        mean = mean / mean[0]
+        ax.errorbar(dist, mean, err, ls="", marker=marker, c=col, label=f"{xlabel} = {x}")
+
+        corrLength, xfit, yfit = fitCorr(dist, mean, err)
+        ax.plot(xfit, yfit, c=col, label=fr"$\xi = {corrLength:.2f}$")
+
+    ax.set_yscale("log")
+    ax.legend()
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
 
 def main():
@@ -86,24 +153,11 @@ def main():
 
     np.random.seed(123)
 
-    (xlabel, x), fixed, ener, magn = loadData(datadir, skip=20)
+    (xlabel, x), fixed, ener, magn, corrs = loadData(datadir, skip=20)
 
-    fig = plt.figure(figsize=(11, 5))
-    fig.suptitle(",   ".join("{}={}".format(n, v) for n, v in fixed.items()))
+    plotEM(xlabel, x, fixed, ener, magn)
+    plotCorr(xlabel, x, fixed, corrs)
 
-    axe = fig.add_subplot(121)
-    axe.set_xlabel(xlabel)
-    axe.set_ylabel(r"$E / \Lambda$")
-    xs, es = xsorted(x, ener)
-    axe.errorbar(xs, *zip(*es), ls="-", marker=".")
-
-    axm = fig.add_subplot(122)
-    axm.set_xlabel(xlabel)
-    axm.set_ylabel(r"$|m|$")
-    xs, ms = xsorted(x, magn)
-    axm.errorbar(xs, *zip(*ms), ls="-", marker=".")
-
-    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
     plt.show()
 
 if __name__ == "__main__":
